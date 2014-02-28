ff4285c43baeb87373b286983ba5acc9570d367b
Mike Regan <dreadknot@gmail.com>
2014-02-28 10:12:37 -0800

    add google python docs

---
+++ pyquick/hello.py
@@ -0,0 +1,16 @@
+import sys
+
+def Hello(name):
+	if name == 'Alice' or name -- 'Nick':
+		name = name + '????'
+	else:
+		print 'Else'
+		DoesNotExist(name)
+	name = name + '!!!!!'
+	print 'Hello', name
+
+def main():
+	Hello("Alice")
+
+if __name__ == '__main__':
+	main()
