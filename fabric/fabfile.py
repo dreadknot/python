7865b800afb3c1f49ff5ced4b16fbd73a177ea8f
Mike Regan <dreadknot@gmail.com>
2014-09-02 15:30:37 -0700

    add fab task to apply splunk license

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -1,7 +1,14 @@
-from fabric.api import local
-from fabric.api import local, settings, abort
+from fabric.api import *
 from fabric.contrib.console import confirm
 
+
+#env.hosts = ['localhost']
+#env.host = "localhost"
+#env.password = "vagrant"
+#env.port = 2212
+#env.user = "vagrant"
+#env.host_string = "vagrant@127.0.0.1:2212"
+#env.no_keys = ['True']
 
 def hello(name="world"):
     print("Hello %s!" % name)
@@ -12,3 +19,23 @@ def test():
         result = local('ls-lsa', capture=True)
     if result.failed and not confirm("Tests failed. Continue anyway?"):
         abort("Aborting at user request.")
+
+def deploy():
+    code_dir = '/srv/django/myproject'
+    with cd(code_dir):
+        run("git pull")
+        run("touch app.wsgi")
+
+def list_license():
+    run("sudo /opt/splunk/bin/splunk list license")
+
+def apply_splunk_license(license, host_string='127.0.0.1', splunk_dir="/opt/splunk"):
+    """Apply a splunk license to a remote host"""
+
+    if license is None:
+	   license = prompt('What license to apply?: ')
+
+    with cd(splunk_dir):
+ 		license_location = put(license)
+ 		for s in license_location:
+ 			run("sudo %s/bin/splunk add license %s" % (splunk_dir, s))
