d06bdf3275885d5da40fc2732b7ed390e537b21f
Mike Regan <dreadknot@gmail.com>
2015-02-15 17:20:53 -0800

    load file print bits

--- bio/open.py
+++ bio/open.py
@@ -1,13 +1,23 @@
 
+import base64
+
 with open('Homo_sapiens.GRCh38.dna.chromosome.1.fa', 'r') as f:
 	for line in f:
 		# print line.rstrip()
+		byte = ''
 		for char in line.rstrip():
 			if char == 'A':
-			    print "01\n"
+			    # print "01\n"
+			    byte = byte + '01'
 			elif char == 'G':
-			    print "10\n"
+			    # print "10\n"
+			    byte = byte + '10'
 			elif char == 'C':
-			    print "11\n"
+			    # print "11\n"
+			    byte + byte + '11'
 			elif  char == 'T':
-			    print "00\n"
+			    # print "00\n"
+			    byte = byte + '00'
+		if len(byte) >= 32:
+			print byte
+
